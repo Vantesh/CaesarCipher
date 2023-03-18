81ffeee71bb1ca9dc77daa2e9615e0963cf5bfe0
Van-tesh <smalletvicky@gmail.com>
2023-03-18 10:58:36 +0300

    tidy up code

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from symbols import characters
+while True:
+  direction= input("Type 'encode' to encrypt or 'decode' to decrypt:  ").lower()
 
-direction= input("Type 'encode' to encrypt or 'decode' to decrypt:  ").lower()
+  text=input("Type Your message:\n\n").lower()
 
-text=input("Type Your message:\n\n").lower()
+  shift=int(input("Type the shift number:\n"))
 
-shift=int(input("Type the shift number:\n"))
-
-def encrypt(plain_text, shift_amount):
-  cipher_text=""
-  for letter in plain_text:
-    position=alphabet.index(letter)
-    new_position=position+shift_amount
-    new_letter=alphabet[new_position]
-    cipher_text+=new_letter
-  print(f"The encrypted message is {cipher_text}")
-
-
-
-def decrypt(plain_text, shift_amount):
-  cipher_text=""
-  for letter in plain_text:
-    position=alphabet.index(letter)
-    new_position=position-shift_amount
-    new_letter=alphabet[new_position]
-    cipher_text+=new_letter
-  print(f"The decrypted message is {cipher_text}")
-  
-
-
-if direction == 'encode':
-  encrypt(plain_text=text , shift_amount=shift)
-elif direction== 'decode':
-  decrypt(plain_text=text , shift_amount=shift)
-else:
-  print("Invalid input")
+  def encrypt(plain_text, shift_amount, direction):
+    cipher_text=""
+    if direction == 'decode':
+      shift_amount*=-1
+    for char in plain_text:
+      if char in characters:
+        position=characters.index(char)
+        new_position=position+shift_amount
+        new_char=characters[new_position]
+        cipher_text+=new_char
+      else:
+        cipher_text+=char
+    print(f"The {direction}d message is {cipher_text}\n")
+  encrypt(plain_text=text , shift_amount=shift, direction=direction)
+  retry = input("retry ? (y/n): ")
+  if retry.lower() != "y":
+    break
